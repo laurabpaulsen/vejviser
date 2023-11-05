e657443ae7a3979a76ed17e6beb3990a27b837ee
laurabpaulsen <202005791@post.au.dk>
2023-11-05 13:14:47 +0100

    Add script to create dictionary of file information for pdfs folder

---
+++ file_info.py
@@ -0,0 +1,24 @@
+"""
+This script creates a dictionary of file information for the files in the pdfs folder.
+
+It holds the start and stop page numbers for each file, as well as the file name.
+"""
+from pathlib import Path
+import ndjson
+
+if __name__ in "__main__":
+    path = Path(__file__).parent
+
+    # dictionary of file names and their start and stop pages
+    file_info = {
+        # 'file_name': [start_page, stop_page]
+        'Kraks Vejviser 1945 gaderegister .pdf': [27, None],
+        'Kraks Vejviser 1944 gaderegister.pdf': [27, 762],
+        "Kraks Vejviser 1943 Gade og hus register.pdf": [27, 739]
+    }
+
+    # save the dictionary to a file
+    with open(path / 'file_info.ndjson', 'w') as f:
+        ndjson.dump(file_info, f)
+
+
